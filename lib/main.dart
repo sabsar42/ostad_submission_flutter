b0e97cca17439d75cb5c83a281307b020df8d11c
Shakib Absar <sabsar42@gmail.com>
2023-09-18 02:34:45 +0600

    not final

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        color: Colors.orange,
-        debugShowCheckedModeBanner: true,
-        home: HomeScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
 
@@ -22,37 +19,86 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        toolbarHeight: 75,
+        toolbarHeight: 70,
         backgroundColor: Colors.blue,
         centerTitle: true,
-        title: Text(
-          'Profile',
+        title: const Text(
+          'Photo Gallery',
           style: TextStyle(color: Colors.white, fontSize: 25),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.account_circle_rounded,
-              color: Colors.green,
-              size: 85,
-            ),
-            Text(
-              'John Doe',
+      body: Scrollbar(
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(1, 20, 20, 10),
+            child: Text(
+              "Welcome To My Photo Gallery!",
               style: TextStyle(
-                color: Colors.green,
-                fontSize: 40,
-              ),
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25),
             ),
-            Text(
-              'Flutter Batch 4',
-              style: TextStyle(color: Colors.lightBlue, fontSize: 30),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Search for Photos',
+                    border: OutlineInputBorder())),
+          ),
+          Expanded(
+            child: GridView(
+              padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 0.9),
+              children: [
+                Image.network('http://thecatapi.com/api/images/get?format=src'),
+                Image.network('http://thecatapi.com/api/images/get?format=src'),
+                Image.network('http://thecatapi.com/api/images/get?format=src'),
+                Image.network('http://thecatapi.com/api/images/get?format=src'),
+                Image.network('http://thecatapi.com/api/images/get?format=src'),
+                Image.network('http://thecatapi.com/api/images/get?format=src'),
+                Image.network('http://thecatapi.com/api/images/get?format=src'),
+                Image.network('http://thecatapi.com/api/images/get?format=src'),
+                Image.network('http://thecatapi.com/api/images/get?format=src')
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+              children: [
+                ListTile(
+                  title: Text('Photo 1'),
+                  subtitle: Text('Photo 1'),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'http://thecatapi.com/api/images/get?format=src'),
+                  ),
+                ),
+                ListTile(
+                  title: Text('Photo 1'),
+                  subtitle: Text('Photo 1'),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'http://thecatapi.com/api/images/get?format=src'),
+                  ),
+                ),
+                ListTile(
+                  title: Text('Photo 1'),
+                  subtitle: Text('Photo 1'),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'http://thecatapi.com/api/images/get?format=src'),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
