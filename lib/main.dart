657105e4d976d356ec3069719d699cc6c52cc317
Shakib Absar <sabsar42@gmail.com>
2023-09-18 13:18:21 +0600

    live test completed - final

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: Scrollbar(
+
         child: Column(children: [
           Padding(
             padding: EdgeInsets.fromLTRB(1, 20, 20, 10),
@@ -46,27 +47,41 @@ class HomeScreen extends StatelessWidget {
                     hintText: 'Search for Photos',
                     border: OutlineInputBorder())),
           ),
-          Expanded(
-            child: GridView(
-              padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 0.9),
-              children: [
-                Image.network('http://thecatapi.com/api/images/get?format=src'),
-                Image.network('http://thecatapi.com/api/images/get?format=src'),
-                Image.network('http://thecatapi.com/api/images/get?format=src'),
-                Image.network('http://thecatapi.com/api/images/get?format=src'),
-                Image.network('http://thecatapi.com/api/images/get?format=src'),
-                Image.network('http://thecatapi.com/api/images/get?format=src'),
-                Image.network('http://thecatapi.com/api/images/get?format=src'),
-                Image.network('http://thecatapi.com/api/images/get?format=src'),
-                Image.network('http://thecatapi.com/api/images/get?format=src')
-              ],
+
+
+
+          GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 5.0,
+              crossAxisSpacing: 5.0,
             ),
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  final snackBar = SnackBar(
+                    content: Text('Image ${index + 1} clicked!'),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                child: Column(
+                  children: [
+                    Image.network(
+                      'http://thecatapi.com/api/images/get?format=src${index + 1}',
+                      width: 100,
+                      height: 100,
+                    ),
+                    SizedBox(height: 5),
+                    Text('Caption ${index + 1}'),
+                  ],
+                ),
+              );
+            },
           ),
+
+
           Expanded(
             child: ListView(
               padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
@@ -98,7 +113,25 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           )
-        ]),
+
+          Container(
+            child: ElevatedButton(
+              onPressed: () {
+                final snackBar = SnackBar(
+                  content: Text('Image clicked!'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              child: Icon(Icons.favorite), // Use a valid Flutter icon here
+            ),
+          )
+
+
+
+
+        ]
+
+        ),
       ),
     );
   }
