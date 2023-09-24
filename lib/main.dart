bcc2a2df903ad2675517aeff04699b237ab8b7ef
Shakib Absar <sabsar42@gmail.com>
2023-09-24 23:34:59 +0600

    mod-7-live-test

--- lib/main.dart
+++ lib/main.dart
@@ -1,163 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: HomeScreen(),
+      home: CounterScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CounterState();
+}
+
+class _CounterState extends State<CounterScreen> {
+  int counter = 0;
+
+  void _showAlertDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Button Pressed $counter times'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        toolbarHeight: 60,
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        title: const Text(
-          'Photo Gallery',
-          style: TextStyle(color: Colors.white, fontSize: 22),
-        ),
+        title: Text('Counter App'),
       ),
-      body: SingleChildScrollView(
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 20, 30, 10),
-              child: Text(
-                "Welcome To My Photo Gallery!",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                ),
+            Text(" Count : "),
+            Text(
+              counter.toString(),
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search for photos...',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 3,
-                crossAxisSpacing: 1,
-
-              ),
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    final snackBar = SnackBar(
-                      content: Text('Photo ${index} clicked!'),
-                      backgroundColor: Colors.green,
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                  child: Column(
-                    children: [
-                      Image.network(
-                        'https://thecatapi.com/api/images/get?format=src&type=${index + 1}',
-                        width: 100,
-                        height: 90,
-                        fit: BoxFit.cover,
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        'Photo ${index}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-            ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.fromLTRB(0, 1, 5, 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ListTile(
-                  title: Text('Photo 1'),
-                  subtitle: Text('Description of photo 1'),
-                  leading: Container(
-                    width: 100,
-                    height: 100,
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundImage: NetworkImage(
-                        'https://thecatapi.com/api/images/get?format=src&type=jpeg',
-                      ),
-                    ),
-                  ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter = counter + 1;
+                      if (counter >= 5) {
+                        _showAlertDialog();
+                      }
+                    });
+                  },
+                  child: Icon(Icons.add),
                 ),
-                ListTile(
-                  title: Text('Photo 2'),
-                  subtitle: Text('Description of photo 2'),
-                  leading: Container(
-                    width: 100,
-                    height: 100,
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundImage: NetworkImage(
-                        'http://thecatapi.com/api/images/get?format=src&type=jpg',
-                      ),
-                    ),
-                  ),
-                ),
-                ListTile(
-                  title: Text('Photo 3'),
-                  subtitle: Text('Description of photo 3'),
-                  leading: Container(
-                    width: 100,
-                    height: 100,
-                    child: CircleAvatar(
-                      radius: 50,
-                      backgroundImage: NetworkImage(
-                        'http://thecatapi.com/api/images/get?format=src&type=img',
-                      ),
-                    ),
-                  ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (counter > 0) {
+                        counter = counter - 1;
+                      }
+                    });
+                  },
+                  child: Icon(Icons.remove),
                 ),
               ],
-            ),
-            Center(
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 150),
-                child: ElevatedButton(
-
-                  onPressed: () {
-                    final snackBar = SnackBar(
-                      content: Text('Photos Uploaded Successfully!'),
-                      backgroundColor: Colors.orange,
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-               
-
-                  child: Icon(Icons.upload,
-                      size: 35), // Use a valid Flutter icon here
-                ),
-              ),
             ),
           ],
         ),
