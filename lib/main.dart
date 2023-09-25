832995fd210182b2f27fafd9ab326fd49610bd2b
Shakib Absar <sabsar42@gmail.com>
2023-09-25 21:30:59 +0600

    Total Count need to change

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,163 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        color: Colors.orange,
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen());
+      home: CounterScreen(),
+    );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CounterState();
+}
+
+class Item {
+  String productName;
+  String productPrice;
+  int counter = 0;
+
+  Item(this.productName, this.productPrice, this.counter);
+}
+
+class _CounterState extends State<CounterScreen> {
+  List<Item> items = [
+    Item('Product 1', '10.00', 0),
+    Item('Product 2', '15.00', 0)
+  ];
+
+  var totalProduct = 0;
+
+  void checkCounterIncrease() {
+    for (int i = 0; i < items.length; i++) {
+      if (items[i].counter > 0) {
+        totalProduct++;
+      }
+    }
+  }
+
+  void _showAlertDialog(int totalCounter, int itemIndex) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Congratulations'),
+          content: Text(
+              'You brought ${items[itemIndex].counter} product of  ${items[itemIndex].productName} '),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        toolbarHeight: 100,
-        backgroundColor: Colors.green,
-        elevation: 70,
+        title: Text('Product List'),
         centerTitle: true,
-        title: Text(
-          'Home',
-          style: TextStyle(
-              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
-        ),
-        leading: Icon(
-          Icons.add_business,
-          color: Colors.white,
-          size: 32,
-        ),
-        actions: [
-          Positioned(
-            left: 50, // Adjust the position as needed
-            child: Icon(
-              Icons.search,
-              color: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(items[index].productName),
+                  subtitle: Text('Price: ${items[index].productPrice}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Column(
+                        children: [
+                          Text('Count: ${items[index].counter.toString()}'),
+                          // SizedBox(height: 3),
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                items[index].counter++;
+                                // int totalCounter = items.fold(0, (sum, item) => sum + item.counter);
+                                if (items[index].counter >= 5) {
+                                  _showAlertDialog(items[index].counter, index);
+                                }
+                              });
+                            },
+                            child: Row(
+                              children: [
+                                Text('Buy Now'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          checkCounterIncrease();
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return CartScreen(productCount: totalProduct);
+            totalProduct=0;
+          }));
+          totalProduct=0;
+        },
+        child: Icon(Icons.shopping_cart),
+
+      ),
+    );
+  }
+}
+
+class CartScreen extends StatelessWidget {
+  final int productCount;
+
+  const CartScreen({super.key, required this.productCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CounterScreen(),
+              ),
+            );
+          },
+          icon: Icon(Icons.arrow_back_outlined),
+        ),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'This is mod 5 assignment',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'My',
-                  style: TextStyle(color: Colors.redAccent, fontSize: 25),
-                ),
-                SizedBox(width: 5),
-                Text(
-                  'Phone',
-                  style: TextStyle(color: Colors.blueAccent, fontSize: 15),
-                ),
-                SizedBox(width: 5),
-                Text(
-                  'name',
-                  style: TextStyle(color: Colors.purpleAccent, fontSize: 20),
-                ),
-                SizedBox(width: 5),
-                Text(
-                  'Your Phone Name',
-                  style: TextStyle(
-                    color: Colors.orangeAccent,
-                    fontSize: 25,
-                  ),
-                ),
-              ],
-            ),
-          ],
+        child: Text(
+          'Total Products :   $productCount ',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
