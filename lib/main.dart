9ee7c9367a683aba70b21f84ada96a59377b4f34
Shakib Absar <sabsar42@gmail.com>
2023-09-30 11:47:04 +0600

    Isuues - - Scroll need to work - Text need to be rapped - Padding should be added

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Orientation Demo'),
+        title: Text('News Feed'),
       ),
       body: LayoutBuilder(
         builder: (context, constraints) {
@@ -39,105 +39,121 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildPortraitLayout() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
+  Widget _buildLandscapeLayout() {
+    return Row(
       children: [
         Container(
           padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
           child: CircleAvatar(
-            radius: 80,
+            radius: 200,
             backgroundImage: NetworkImage(
               'https://media.istockphoto.com/id/186985453/photo/old-folding-camera.webp?s=170667a&w=0&k=20&c=E87gjPz6vFNEpvxWJ_lWRycPPc8qaR_5WD-DzPEcH4E=',
             ),
           ),
         ),
-        GridView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisSpacing: 3,
-            crossAxisSpacing: 1,
-          ),
-          itemCount: 6,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                // Handle onTap action
-              },
-              child: Column(
-                children: [
-                  Image.network(
-                    'https://media.istockphoto.com/id/186985453/photo/old-folding-camera.webp?s=170667a&w=0&k=20&c=E87gjPz6vFNEpvxWJ_lWRycPPc8qaR_5WD-DzPEcH4E=',
-                    width: 100,
-                    height: 90,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    'Photo ${index}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Jhone Doe',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            );
-          },
+              SizedBox(height: 8),
+              Text(
+                'In the heart of a bustling city, life moves at an exhilarating pace.'
+                'In the heart of a bustling city, life moves at an exhilarating pace.'
+                'In the heart of a bustling city, life moves at an exhilarating pace.',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(height: 8),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 3,
+                    crossAxisSpacing: 1,
+                  ),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1555652736-e92021d28a10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+                        width: 150,
+                        height: 150,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
   }
 
-  Widget _buildLandscapeLayout() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
+  Widget _buildPortraitLayout() {
+    return Column(
       children: [
         Container(
           padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
           child: CircleAvatar(
-            radius: 80,
+            radius: 200,
             backgroundImage: NetworkImage(
               'https://media.istockphoto.com/id/186985453/photo/old-folding-camera.webp?s=170667a&w=0&k=20&c=E87gjPz6vFNEpvxWJ_lWRycPPc8qaR_5WD-DzPEcH4E=',
             ),
           ),
         ),
-        GridView.builder(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            mainAxisSpacing: 3,
-            crossAxisSpacing: 1,
-          ),
-          itemCount: 6,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                // Handle onTap action
-              },
-              child: Column(
-                children: [
-                  Image.network(
-                    'https://media.istockphoto.com/id/186985453/photo/old-folding-camera.webp?s=170667a&w=0&k=20&c=E87gjPz6vFNEpvxWJ_lWRycPPc8qaR_5WD-DzPEcH4E=',
-                    width: 100,
-                    height: 90,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    'Photo ${index}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Jhone Doe',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            );
-          },
+              SizedBox(height: 8),
+              Text(
+                'In the heart of a bustling city, life moves at an exhilarating pace.'
+                'In the heart of a bustling city, life moves at an exhilarating pace.'
+                'In the heart of a bustling city, life moves at an exhilarating pace.',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(height: 8),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 3,
+                    crossAxisSpacing: 1,
+                  ),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1555652736-e92021d28a10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+                        width: 150,
+                        height: 150,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
