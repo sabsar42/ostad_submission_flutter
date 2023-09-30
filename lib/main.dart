d43256927a054662c009c57dfc73f93d2af44303
Shakib Absar <sabsar42@gmail.com>
2023-09-30 20:18:23 +0600

    Update main.dart
    Check

--- lib/main.dart
+++ lib/main.dart
@@ -40,122 +40,189 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _buildLandscapeLayout() {
-    return Row(
-      children: [
-        Container(
-          padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
-          child: CircleAvatar(
-            radius: 200,
-            backgroundImage: NetworkImage(
-              'https://media.istockphoto.com/id/186985453/photo/old-folding-camera.webp?s=170667a&w=0&k=20&c=E87gjPz6vFNEpvxWJ_lWRycPPc8qaR_5WD-DzPEcH4E=',
-            ),
-          ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Jhone Doe',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'In the heart of a bustling city, life moves at an exhilarating pace.'
-                'In the heart of a bustling city, life moves at an exhilarating pace.'
-                'In the heart of a bustling city, life moves at an exhilarating pace.',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-                textAlign: TextAlign.justify,
-              ),
-              SizedBox(height: 8),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 3,
-                    crossAxisSpacing: 1,
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              margin: EdgeInsets.all(20),
+              child: Container(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                child: CircleAvatar(
+                  radius: 200,
+                  backgroundImage: NetworkImage(
+                    'https://media.istockphoto.com/id/186985453/photo/old-folding-camera.webp?s=170667a&w=0&k=20&c=E87gjPz6vFNEpvxWJ_lWRycPPc8qaR_5WD-DzPEcH4E=',
                   ),
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Image.network(
-                        'https://images.unsplash.com/photo-1555652736-e92021d28a10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
-                        width: 150,
-                        height: 150,
-                      ),
-                    );
-                  },
                 ),
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(top: 10, right: 10, left: 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        'Jhone Doe',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Wrap(
+                            children: [
+                              Text(
+                                'In the heart of a bustling city, life moves at an exhilarating pace.'
+                                'In the heart of a bustling city, life moves at an exhilarating pace.'
+                                'In the heart of a bustling city, life moves at an exhilarating pace.',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          SizedBox(
+                            height: 700,
+                            width: 1000,
+                            child: Container(
+                              child: GridView.builder(
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3,
+                                  mainAxisSpacing: 3,
+                                  crossAxisSpacing: 1,
+                                ),
+                                itemCount: 10,
+                                itemBuilder: (context, index) {
+                                  return Card(
+                                    child: Image.network(
+                                      'https://images.unsplash.com/photo-1555652736-e92021d28a10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+                                      width: 150,
+                                      height: 150,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 
   Widget _buildPortraitLayout() {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
-          child: CircleAvatar(
-            radius: 200,
-            backgroundImage: NetworkImage(
-              'https://media.istockphoto.com/id/186985453/photo/old-folding-camera.webp?s=170667a&w=0&k=20&c=E87gjPz6vFNEpvxWJ_lWRycPPc8qaR_5WD-DzPEcH4E=',
-            ),
-          ),
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Jhone Doe',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'In the heart of a bustling city, life moves at an exhilarating pace.'
-                'In the heart of a bustling city, life moves at an exhilarating pace.'
-                'In the heart of a bustling city, life moves at an exhilarating pace.',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-                textAlign: TextAlign.justify,
-              ),
-              SizedBox(height: 8),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 3,
-                    crossAxisSpacing: 1,
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Container(
+        margin: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              margin: EdgeInsets.all(20),
+              child: Container(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                child: CircleAvatar(
+                  radius: 200,
+                  backgroundImage: NetworkImage(
+                    'https://media.istockphoto.com/id/186985453/photo/old-folding-camera.webp?s=170667a&w=0&k=20&c=E87gjPz6vFNEpvxWJ_lWRycPPc8qaR_5WD-DzPEcH4E=',
                   ),
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      child: Image.network(
-                        'https://images.unsplash.com/photo-1555652736-e92021d28a10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
-                        width: 150,
-                        height: 150,
-                      ),
-                    );
-                  },
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    child: Text(
+                      'Jhone Doe',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Wrap(
+                          children: [
+                            Text(
+                              'In the heart of a bustling city, life moves at an exhilarating pace.'
+                              'In the heart of a bustling city, life moves at an exhilarating pace.'
+                              'In the heart of a bustling city, life moves at an exhilarating pace.',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        SizedBox(
+                          height: 700,
+                          width: 1000,
+                          child: Container(
+                            child: GridView.builder(
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3,
+                                mainAxisSpacing: 3,
+                                crossAxisSpacing: 1,
+                              ),
+                              itemCount: 10,
+                              itemBuilder: (context, index) {
+                                return Card(
+                                  child: Image.network(
+                                    'https://images.unsplash.com/photo-1555652736-e92021d28a10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+                                    width: 150,
+                                    height: 150,
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
