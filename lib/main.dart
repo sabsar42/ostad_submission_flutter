bdceb9aa0e68770a6656710ef90cc956bc5c7aff
Shakib Absar <sabsar42@gmail.com>
2023-10-13 23:37:12 +0600

    Completed - finally

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});
 
@@ -24,168 +22,68 @@ class ColorChangeScreen extends StatefulWidget {
 }
 
 class _ColorChangeScreenState extends State<ColorChangeScreen> {
-  String choose = '';
+  final List<String> items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+  ];
 
-  void changeColor(String size) {
-    setState(() {
-      choose = size;
-    });
+  List<String> selectedItems = [];
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Selected Size: $size'),
-      ),
-    );
-  }
-
-  Color getButtonColor(String size) {
-    if (choose == size) {
-      return Colors.orange;
-    } else
-      return Colors.grey;
+  void Selected(bool isSelected, int index) {
+    if (isSelected) {
+      selectedItems.remove(items[index]);
+    } else {
+      selectedItems.add(items[index]);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Size Selector'),
+        title: Text('Selection Builder '),
       ),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        padding: EdgeInsets.all(30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    changeColor('S');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: getButtonColor('S'),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          bool isSelected = selectedItems.contains(items[index]);
+          return ListTile(
+            title: Text(items[index]),
+            tileColor: isSelected ? Colors.blue : null,
+            onTap: () {
+              setState(() {
+                Selected(isSelected, index);
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text('Selected Items'),
+                content:
+                    Text('Number of selected items: ${selectedItems.length}'),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Close'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
                   ),
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text(
-                      'S',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    changeColor('M');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: getButtonColor('M'),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text(
-                      'M',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    changeColor('L');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: getButtonColor('L'),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text(
-                      'L',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    changeColor('XL');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: getButtonColor('XL'),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text(
-                      'XL',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    changeColor('XXL');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: getButtonColor('XXL'),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text(
-                      'XXL',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    changeColor('XXXL');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: getButtonColor('XXXL'),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text(
-                      'XXXL',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.check),
       ),
     );
   }
