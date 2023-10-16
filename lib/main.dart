e1a3cc5d2da3970e957c37679764c03df2da3375
Shakib Absar <sabsar42@gmail.com>
2023-10-16 18:21:25 +0600

    Completed - finally

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,225 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class ToDo {
+  String title;
+  String description;
+
+  ToDo(this.title, this.description);
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ColorChangeScreen(),
+      home: ToDoListScreen(),
     );
   }
 }
 
-class ColorChangeScreen extends StatefulWidget {
+class ToDoListScreen extends StatefulWidget {
   @override
-  _ColorChangeScreenState createState() => _ColorChangeScreenState();
+  _ToDoListScreenState createState() => _ToDoListScreenState();
 }
 
-class _ColorChangeScreenState extends State<ColorChangeScreen> {
-  final List<String> items = [
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
-    'Item 5',
-    'Item 6',
-  ];
+class _ToDoListScreenState extends State<ToDoListScreen> {
+  List<ToDo> toDoList = [];
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController editTitleController = TextEditingController();
+  final TextEditingController editDescriptionController = TextEditingController();
 
-  List<String> selectedItems = [];
+  void _addTask() {
+    String title = titleController.text;
+    String description = descriptionController.text;
 
-  void Selected(bool isSelected, int index) {
-    if (isSelected) {
-      selectedItems.remove(items[index]);
-    } else {
-      selectedItems.add(items[index]);
+    if (title.isNotEmpty && description.isNotEmpty) {
+      setState(() {
+        toDoList.add(ToDo(title, description));
+        titleController.clear();
+        descriptionController.clear();
+      });
     }
+  }
+
+  void _deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
+  void _editTask(BuildContext context, int index) {
+    ToDo selectedTask = toDoList[index];
+    titleController.text = selectedTask.title;
+    descriptionController.text = selectedTask.description;
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),// Increased bottom padding
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      hintText: "Add Title",
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(
+                      labelText: 'Add Description',
+                      hintText: "Add Description",
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedTask.title = titleController.text;
+                      selectedTask.description = descriptionController.text;
+                      Navigator.of(context).pop();
+                    });
+                  },
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(30, 10, 30,10)), // Add padding here
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                  ),
+                  child: Text('ADD'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Selection Screen '),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Icon(
+              size: 30.0,
+              Icons.search,
+              color: Colors.black,
+            ),
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          bool isSelected = selectedItems.contains(items[index]);
-          return ListTile(
-            title: Text(items[index]),
-            tileColor: isSelected ? Colors.blue : null,
-            onTap: () {
-              setState(() {
-                Selected(isSelected, index);
-              });
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text('Selected Items'),
-                content:
-                    Text('Number of selected items: ${selectedItems.length}'),
-                actions: <Widget>[
-                  TextButton(
-                    child: Text('Close'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
+      body: Column(
+        children: [
+
+          Container(
+            margin: EdgeInsets.all(5),
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: 'Title',
+                hintText: "Add Title",
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(5),
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(
+                labelText: 'Add Description',
+                hintText: 'Add Description',
+                enabledBorder: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          ElevatedButton(
+
+            onPressed: _addTask,
+            child: Text(
+              'ADD',
+              style: TextStyle(fontSize: 20.0), // Increase the font size to make it bigger
+            ),
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(30, 10, 30,10)), // Add padding here
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+          ),
+
+
+          Expanded(
+            child: ListView.separated(
+              itemCount: toDoList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(toDoList[index].title),
+                  subtitle: Text(toDoList[index].description),
+                  trailing: Icon(Icons.arrow_forward),
+                  onLongPress: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+
+                          title: Text("Edit or Delete?"),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () { Navigator.of(context).pop();
+                                _editTask(context,index);
+
+                              },
+                              child: Text("Edit"),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                _deleteTask(index);
+                                Navigator.of(context).pop();
+                              },
+                              child: Text("Delete"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                );
+              }, separatorBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 2.0, // Adjust the height as needed
+                color: Colors.grey, // Set the color of the divider
+                margin: EdgeInsets.symmetric(horizontal: 16.0), // Add some margin
               );
             },
-          );
-        },
-        child: Icon(Icons.check),
+            ),
+          ),
+        ],
       ),
     );
   }
